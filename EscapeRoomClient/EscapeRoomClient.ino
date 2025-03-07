1843c0d7eed97107f614da771851c48ec58df285
Nerya253 <neryalevi253@gmail.com>
2025-03-07 17:18:29 +0200

    client files
    add client files and start wifi connect to other arduino

---
+++ EscapeRoomClient/EscapeRoomClient.ino
@@ -0,0 +1,9 @@
+void setup() {
+  Serial.begin(9600);
+  wifi_Setup();
+}
+
+
+void loop() {
+
+}
---
+++ EscapeRoomClient/WIFIclient.ino
@@ -0,0 +1,39 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <WiFiUdp.h>
+#include <ESP8266HTTPClient.h>
+
+const char* ssid = "Project";
+const char* pswd = "88888888";
+
+WiFiClient client;
+int server_port = 80; 
+
+unsigned long previousMillis = 0;
+
+void wifi_Setup() {
+  Serial.println("wifiSetup");
+  WiFi.begin(ssid, pswd);
+  previousMillis = millis();
+
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.println("trying ...");
+    if(millis() - previousMillis >= 100){
+      previousMillis = millis();
+    }
+   
+  }
+  Serial.println("Connected to network");
+
+  SendData();
+}
+
+
+void SendData(int digit) {
+  HTTPClient http;
+  http.begin(client, "http://55.55.55.55/addDigit?digit=" + digit);
+  int httpCode = http.GET();
+  Serial.println(httpCode);
+  http.end();
+}
+
