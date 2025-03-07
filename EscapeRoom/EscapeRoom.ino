60dc031bd70b922e95e1375a3998c652c65ff025
Nerya253 <neryalevi253@gmail.com>
2025-03-07 17:19:23 +0200

    lock pin setting and door opening function
    lock pin setting and door opening function

--- EscapeRoom/EscapeRoom.ino
+++ EscapeRoom/EscapeRoom.ino
@@ -1,7 +1,16 @@
+#define LOCK_PIN D1   
+
 void setup() {
   Serial.begin(9600);
+  pinMode(LOCK_PIN, OUTPUT);
+  digitalWrite(LOCK_PIN, LOW);  
+  WiFi_setup();
 }
 
 void loop() {
   WiFi_loop();
+}
+
+void unlockDoor() {
+  digitalWrite(LOCK_PIN, HIGH);
 }
