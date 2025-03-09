8f916a56c7d3414b55d22a147012c51afc620041
Nerya253 <neryalevi253@gmail.com>
2025-03-09 22:48:30 +0200

    Adding the third puzzle - a game of lights
    Adding the third puzzle - a game of lights
    Currently with 3 LEDs and buttons

--- EscapeRoomClient/EscapeRoomClient.ino
+++ EscapeRoomClient/EscapeRoomClient.ino
@@ -1,15 +1,23 @@
 #include <DHT_U.h>
 #include <DHT.h>
 
-#define FUN D4
+#define FUN D1
 
-#define DHTPIN D1
+#define DHTPIN D4
 #define DHTTYPE DHT11
 
 #define pinMuxA D5
 #define pinMuxB D6
 #define pinMuxC D7
 #define pinMuxInOut A0
+
+#define R_LED D1
+#define G_LED D2
+#define B_LED D3
+
+#define R_BTN D5
+#define G_BTN D6
+#define B_BTN D7
 
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -19,36 +27,97 @@ void SendCodeDigit(int puzzleNumber);
 void puzzleSolved(int puzzleNumber);
 void wifi_Setup();
 
-
-//חידת אור
+int currentPuzzle = 0;
 unsigned long currentLightMillis = 0;
 unsigned long lastSensorReadMillis = 0;
+
+//חידה 1 - חיישן אור
 unsigned long lightCorrectStartTime = 0;
 int maxLightLevel = 0;
-int currentPuzzle = 0;
 bool inCorrectLightRange = false;
 bool samplingMaxLight = true;
 unsigned long samplingStartTime = 0;
+
+//חידה 2 - טמפרטורה
+float currentTemp = 0;
+float targetTemp = 0;
+bool tempPuzzleSolved = false;
+bool tempInCorrectRange = false;
+unsigned long tempCorrectStartTime = 0;
+static unsigned long lastTempCheckMillis = 0;
+
+//חידה 3 - משחק אורות
+const int sequenceLength = 4;
+int sequence[sequenceLength];
+int leds[3] = { R_LED, G_LED, B_LED };
+int buttons[3] = { R_BTN, G_BTN, B_BTN };
+int currentStep = 0;
+bool displayingSequence = true;
+unsigned long lastChangeTime = 0;
+int displayState = 0;
+
+unsigned long buttonReleaseTime = 0;
+bool waitingForButtonRelease = false;
+int lastPressedButton = -1;
+
+// משתנים לאפקטים
+bool isBlinking = false;
+int blinkCount = 0;
+unsigned long blinkTime = 0;
+int blinkState = 0;
+
+bool showingVictory = false;
+int victoryStep = 0;
+unsigned long victoryTime = 0;
+
+bool showingStartSequence = true;
+int startSequenceCount = 0;
+unsigned long startSequenceTime = 0;
 
 
 void setup() {
   Serial.begin(9600);
   wifi_Setup();
+
+  pinMode(A0, INPUT);
   pinMode(pinMuxA, OUTPUT);
   pinMode(pinMuxB, OUTPUT);
   pinMode(pinMuxC, OUTPUT);
   pinMode(pinMuxInOut, INPUT);
+
   pinMode(FUN, OUTPUT);
+
+  pinMode(R_LED, OUTPUT);
+  pinMode(G_LED, OUTPUT);
+  pinMode(B_LED, OUTPUT);
+  pinMode(R_BTN, INPUT_PULLUP);
+  pinMode(G_BTN, INPUT_PULLUP);
+  pinMode(B_BTN, INPUT_PULLUP);
+
   digitalWrite(FUN, HIGH);
   dht.begin();
 
+  digitalWrite(A0, LOW); 
+
+  digitalWrite(R_LED, LOW);
+  digitalWrite(G_LED, LOW);
+  digitalWrite(B_LED, LOW);
+
+  for (int i = 0; i < 10; i++) {
+    analogRead(A0);
+  }
+
+  generateRandomSequence();
+
   samplingStartTime = millis();
+  
+  showingStartSequence = true;
+  startSequenceCount = 0;
+  startSequenceTime = millis();
 }
 
 void loop() {
   currentLightMillis = millis();
-
-
   static unsigned long statusPrintTimer = 0;
   if (currentLightMillis - statusPrintTimer >= 5000) {
     statusPrintTimer = currentLightMillis;
@@ -77,7 +146,6 @@ void loop() {
 }
 
 int ReadMuxChannel(byte chnl) {
-
   int a = (bitRead(chnl, 0) > 0) ? HIGH : LOW;
   int b = (bitRead(chnl, 1) > 0) ? HIGH : LOW;
   int c = (bitRead(chnl, 2) > 0) ? HIGH : LOW;
@@ -102,10 +170,6 @@ void handleLightPuzzle() {
 
     if (currentLightMillis - samplingStartTime >= 5000) {
       samplingMaxLight = false;
-      Serial.print("דגימת האור הסתיימה. רמת אור מקסימלית: ");
-      Serial.println(maxLightLevel);
-      Serial.print("רמת אור יעד (80%): ");
-      Serial.println(maxLightLevel * 0.8);
     }
     return;
   }
@@ -113,7 +177,14 @@ void handleLightPuzzle() {
   if (lightLevel > maxLightLevel) {
     maxLightLevel = lightLevel;
   }
-
+  
+  Serial.print("רמת אור: ");
+  Serial.print(lightLevel);
+  Serial.print(" רמת אור מקסימלית: ");
+  Serial.print(maxLightLevel);
+  Serial.print(" רמת אור יעד (80%): ");
+  Serial.println(maxLightLevel * 0.8);
+  
   if (lightLevel <= maxLightLevel * 0.8) {
     if (!inCorrectLightRange) {
       inCorrectLightRange = true;
@@ -134,14 +205,6 @@ void handleLightPuzzle() {
     }
   }
 }
-
-//חידת טמפרטורה
-float currentTemp = 0;
-float targetTemp = 0;
-bool tempPuzzleSolved = false;
-bool tempInCorrectRange = false;
-unsigned long tempCorrectStartTime = 0;
-static unsigned long lastTempCheckMillis = 0;
 
 void handleTemperaturePuzzle() {
   if (currentLightMillis - lastTempCheckMillis >= 2000) {
@@ -180,9 +243,191 @@ void handleTemperaturePuzzle() {
   }
 }
 
+void generateRandomSequence() {
+  long randSeed = millis() * analogRead(A0);
+  randomSeed(randSeed);
+
+  for (int i = 0; i < sequenceLength; i++) {
+    sequence[i] = random(0, 3);
+  }
+
+  Serial.println("New random sequence:");
+  for (int i = 0; i < sequenceLength; i++) {
+    Serial.print(sequence[i]);
+    Serial.print(" ");
+  }
+  Serial.println();
+}
+
 void handleLEDSequencePuzzle() {
-  Serial.println("בדיקת מנורות");
-  delay(5000);
+  if (showingStartSequence) {
+    handleStartSequence();
+    return;
+  }
+  
+  if (isBlinking) {
+    handleErrorBlink();
+    return;
+  }
+  
+  if (showingVictory) {
+    handleVictoryAnimation();
+    return;
+  }
+  
+  if (waitingForButtonRelease) {
+    if (digitalRead(buttons[lastPressedButton]) == HIGH) {
+      digitalWrite(leds[lastPressedButton], LOW); 
+      waitingForButtonRelease = false;
+      buttonReleaseTime = millis();
+    }
+    return;
+  }
+  
+  if (lastPressedButton != -1 && millis() - buttonReleaseTime < 200) {
+    return;
+  }
+  
+  lastPressedButton = -1;
+
+  if (displayingSequence) {
+    unsigned long currentTime = millis();
+    if (currentTime - lastChangeTime > 400) {
+      lastChangeTime = currentTime;
+
+      digitalWrite(R_LED, LOW);
+      digitalWrite(G_LED, LOW);
+      digitalWrite(B_LED, LOW);
+
+      if (displayState == 0) {
+        if (currentStep < sequenceLength) {
+          digitalWrite(leds[sequence[currentStep]], HIGH);
+        }
+        displayState = 1;
+      } else {
+        currentStep++;
+        displayState = 0;
+
+        if (currentStep >= sequenceLength) {
+          displayingSequence = false;
+          currentStep = 0;
+        }
+      }
+    }
+  }
+  else {
+    for (int i = 0; i < 3; i++) {
+      if (digitalRead(buttons[i]) == LOW) {
+        digitalWrite(leds[i], HIGH);
+        lastPressedButton = i;
+        waitingForButtonRelease = true;
+
+        if (i == sequence[currentStep]) {
+          currentStep++;
+          if (currentStep >= sequenceLength) {
+            Serial.println("החידה נפתרה!");
+            showingVictory = true;
+            victoryStep = 0;
+            victoryTime = millis();
+            puzzleSolved(2); 
+            currentPuzzle++;
+          }
+        } else {
+          isBlinking = true;
+          blinkCount = 0;
+          blinkState = 0;
+          blinkTime = millis();
+        }
+        
+        break;
+      }
+    }
+  }
+}
+
+void handleStartSequence() {
+  if (millis() - startSequenceTime > 300) {
+    startSequenceTime = millis();
+    
+    if (blinkState == 0) {
+      digitalWrite(R_LED, HIGH);
+      digitalWrite(G_LED, HIGH);
+      digitalWrite(B_LED, HIGH);
+      blinkState = 1;
+    } else {
+      digitalWrite(R_LED, LOW);
+      digitalWrite(G_LED, LOW);
+      digitalWrite(B_LED, LOW);
+      blinkState = 0;
+      startSequenceCount++;
+      
+      if (startSequenceCount >= 3) {
+        showingStartSequence = false;
+        displayingSequence = true;
+        currentStep = 0;
+        displayState = 0;
+        lastChangeTime = millis();
+      }
+    }
+  }
+}
+
+void handleErrorBlink() {
+  if (millis() - blinkTime > 200) {
+    blinkTime = millis();
+    
+    if (blinkState == 0) {
+      digitalWrite(R_LED, HIGH);
+      digitalWrite(G_LED, HIGH);
+      digitalWrite(B_LED, HIGH);
+      blinkState = 1;
+    } else {
+      digitalWrite(R_LED, LOW);
+      digitalWrite(G_LED, LOW);
+      digitalWrite(B_LED, LOW);
+      blinkState = 0;
+      blinkCount++;
+      
+      if (blinkCount >= 3) {
+        isBlinking = false;
+        currentStep = 0;
+        displayingSequence = true;
+        displayState = 0;
+        lastChangeTime = millis();
+      }
+    }
+  }
+}
+
+void handleVictoryAnimation() {
+  if (millis() - victoryTime > 150) {
+    victoryTime = millis();
+    
+    digitalWrite(R_LED, LOW);
+    digitalWrite(G_LED, LOW);
+    digitalWrite(B_LED, LOW);
+    
+    if (victoryStep < 9) {
+      int ledIndex = victoryStep % 3;
+      digitalWrite(leds[ledIndex], HIGH);
+    }
+    
+    victoryStep++;
+    
+    if (victoryStep >= 9) {
+      showingVictory = false;
+      
+      digitalWrite(R_LED, LOW);
+      digitalWrite(G_LED, LOW);
+      digitalWrite(B_LED, LOW);
+      
+      generateRandomSequence();
+      
+      showingStartSequence = true;
+      startSequenceCount = 0;
+      startSequenceTime = millis();
+    }
+  }
 }
 
 void handleDistancePuzzle() {
